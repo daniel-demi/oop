27a0791469347e6e1755046cd706ea5019a0ae71
daniel-demi <74015981+daniel-demi@users.noreply.github.com>
2021-05-20 16:14:40 +0300

    add tests

---
+++ HW3/OOP/Tests/ExampleTest1.java
@@ -0,0 +1,482 @@
+
+package OOP.Tests;
+
+import OOP.Provided.IllegalBindException;
+import OOP.Solution.Injector;
+import OOP.Solution.Inject;
+
+import OOP.Solution.Named;
+import OOP.Solution.Provides;
+import org.junit.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static java.lang.System.exit;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    static class Superclazz {
+    }
+
+    static class Subclazz extends Superclazz {
+        @Inject
+        public Subclazz() {
+        }    // This constructor should be called because it is injected
+
+        @Inject
+        public void shuga() {
+           // System.out.println("damn honey");
+        }
+
+        @Inject
+        private void dontprintme() {
+            //System.out.println("why was i printed?");
+        }
+
+
+        public Subclazz(Object o) {
+        }    // This one exists but isn't injected
+    }
+
+    static class myClass {
+    }
+
+    static class mySub extends myClass {
+        @Inject
+        public mySub() {
+        }    // This constructor should be called because it is injected
+
+
+        @Inject
+        public void thisisgood(Superclazz A) {
+          //  System.out.println("x is:");
+          //  System.out.println("did i print?");
+        }
+
+        @Inject
+        private void dontprintmeeveragain() {
+       //     System.out.println("stop addressing private methods");
+        }
+
+
+        public mySub(Object o) {
+        }    // This one exists but isn't injected
+    }
+
+    @Test
+    public void test_anonymous() {
+
+        Injector i = new Injector();
+        try {
+            i.bind(Superclazz.class, Subclazz.class);
+            Object o = i.construct(Superclazz.class);
+            assertEquals(o.getClass(), Subclazz.class);
+            i.bind(myClass.class, mySub.class);
+            o = i.construct(myClass.class);
+
+            // System.out.println(o);      // Should be instance of Subclazz
+
+        } catch (Exception e) {
+            System.out.println(("we goofed1"));
+            e.printStackTrace();
+        }
+
+        Injector i2 = new Injector() {
+            @Provides
+            @Message
+            // Now if someone will request a @Message they will receive this message
+            public String provideMessage() {
+              //  System.out.println("x is :");
+                return "Hello, world!";
+            }
+        };
+
+        try {
+            MessagePrinter mp = (MessagePrinter) i2.construct(MessagePrinter.class);
+            mp.print();
+        } catch (Exception e) {
+            System.out.println(("we goofed2"));
+            e.printStackTrace();
+        }
+        new MessagePrinter("@34");
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.PARAMETER, ElementType.METHOD})
+    @interface Message {
+    }
+
+    static class MessagePrinter {
+        private final String message;
+
+        // This constructor is chosen because it is injected
+        // In addition, there's a @Message parameter therefore the injector
+        // searches for a suitable @Provider.
+        // It will find the provideMessage() method on lines 36-41 and call it
+        // Later, when print() is called, MessagePrinter will print "Hello, world!"
+        @Inject
+        public MessagePrinter(@Message String message) {
+            this.message = message;
+        }
+
+        public void print() {
+            System.out.println(this.message);
+        }
+    }
+
+
+    @Test
+    public void test_bind_0() {
+        Injector i = new Injector();
+        assertDoesNotThrow(() -> {
+            i.bind(Superclazz.class, Subclazz.class);
+            Object o = i.construct(Superclazz.class);
+            assertEquals(o.getClass(), Subclazz.class);
+        });
+
+        assertDoesNotThrow(() -> {
+            i.bindToInstance(Class.class, Subclazz.class);
+            Object o = i.construct(Class.class);
+            assertEquals(o, Subclazz.class);
+        });
+
+        assertThrows(IllegalBindException.class, () -> i.bind(Subclazz.class, Superclazz.class));
+
+        //assertThrows(IllegalBindException.class, () -> i.bind(Superclazz.class, Superclazz.class));
+        //assertThrows(IllegalBindException.class, () -> i.bind(Subclazz.class, Subclazz.class));
+    }
+
+    static class Identified {
+        static int countInstances = 0;
+        int id;
+
+        public Identified() {
+            this.id = countInstances++;
+        }
+
+        static void reset() {
+            countInstances = 0;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (!Identified.class.isAssignableFrom(other.getClass())) {
+                return false;
+            }
+
+            return this.id == ((Identified) other).id;
+        }
+    }
+
+    @Test
+    public void test_instance_0() {
+        Injector i = new Injector();
+        assertThrows(IllegalBindException.class, () -> i.bindToInstance(Identified.class, 0));
+        assertThrows(IllegalBindException.class, () -> i.bindToInstance(Identified.class, null));
+        assertThrows(IllegalBindException.class, () -> i.bindToInstance(Identified.class, "hi"));
+        assertThrows(IllegalBindException.class, () -> i.bindToInstance(Identified.class, new Subclazz()));
+        assertThrows(IllegalBindException.class, () -> i.bindToInstance(Identified.class, Identified.class));
+        assertThrows(IllegalBindException.class, () -> i.bindToInstance(Identified.class, Class.class));
+        assertThrows(IllegalBindException.class, () -> i.bindToInstance(Identified.class, Object.class));
+    }
+
+    @Test
+    public void test_instance_1() {
+        assertDoesNotThrow(() -> {
+            Injector i = new Injector();
+            Identified inst = new Identified();
+            i.bindToInstance(Identified.class, inst);
+            for (int j = 0; j < 10; j++)
+                assertEquals(inst, i.construct(Identified.class));
+
+            Identified another = new Identified();
+            assertNotEquals(another, inst);
+            assertNotEquals(another, i.construct(Identified.class));
+
+            i.bindToInstance(Identified.class, another);
+            for (int j = 0; j < 10; j++) {
+                assertEquals(another, i.construct(Identified.class));
+                assertNotEquals(inst, i.construct(Identified.class));
+            }
+        });
+    }
+
+    void test_supplier(Injector i) {
+        Supplier<Object> sup = Identified::new;
+        i.bindToSupplier(Identified.class, sup);
+
+        for (int k = 0; k < 3; k++) {
+            Identified.reset();
+            for (int j = 0; j < 10; j++) {
+                int finalJ = j;
+                assertDoesNotThrow(() -> {
+                    Identified output = (Identified) i.construct(Identified.class);
+                    assertEquals(finalJ, output.id);
+                });
+            }
+        }
+    }
+
+    void test_instance(Injector i) {
+        assertDoesNotThrow(() -> {
+            Identified inst = new Identified();
+            i.bindToInstance(Identified.class, inst);
+            for (int j = 0; j < 10; j++) {
+                assertEquals(inst, i.construct(Identified.class));
+            }
+        });
+    }
+
+    @Test
+    public void test_supplier_1() {
+        assertDoesNotThrow(() -> {
+            Injector i = new Injector();
+            Supplier<Object> sup = Identified::new;
+            i.bindToSupplier(Identified.class, sup);
+
+            for (int k = 0; k < 3; k++) {
+                Identified.reset();
+                for (int j = 0; j < 10; j++) {
+                    Identified output = (Identified) i.construct(Identified.class);
+                    assertEquals(j, output.id);
+                }
+            }
+
+            Identified inst = new Identified();
+            i.bindToInstance(Identified.class, inst);
+            for (int j = 0; j < 10; j++) {
+                assertEquals(inst, i.construct(Identified.class));
+            }
+
+        });
+    }
+
+
+    @Test
+    public void test_provided() {
+
+        Injector i = new Injector();
+        try {
+            i.bind(Superclazz.class, Subclazz.class);
+            Object o = i.construct(Superclazz.class);
+            assertEquals(o.getClass(), Subclazz.class);
+            // System.out.println(o);      // Should be instance of Subclazz
+
+        } catch (Exception e) {
+            System.out.println(("we goofed"));
+            e.printStackTrace();
+        }
+
+        Injector i2 = new Injector() {
+            @Provides
+            @Message
+            // Now if someone will request a @Message they will receive this message
+            public String provideMessage() {
+                return "Hello, world!";
+            }
+        };
+
+        try {
+            MessagePrinter mp = (MessagePrinter) i2.construct(MessagePrinter.class);
+            mp.print();
+        } catch (Exception e) {
+            System.out.println(("we goofed"));
+            e.printStackTrace();
+        }
+        new MessagePrinter("@34");
+    }
+
+
+    /////////////// Provides
+    //@Test
+//    public void test_provides_0(){
+//        Injector i = new Injector(){
+//            @Provides
+//            Integer IntegerProvider(){
+//                return 0;
+//            }
+//        };
+//
+//        assertDoesNotThrow(()->
+//                assertEquals(0, i.construct(Integer.class))
+//        );
+//    }
+
+    static class AnyInteger {
+        int x;
+
+        public AnyInteger() {
+            this.x = 9999;
+        }
+
+        @Inject
+        AnyInteger(@Named(name = "Zero") AnyInteger other) {
+            this.x = other.x;
+        }
+    }
+
+    static class JustZero extends AnyInteger {
+        JustZero() {
+            this.x = 0;
+            // System.out.println("i got here");}
+        }
+    }
+
+
+    @Test
+    public void test_named_0() {
+        Injector i = new Injector();
+        i.bindByName("Zero", JustZero.class);
+
+        assertDoesNotThrow(() -> {
+            i.construct(AnyInteger.class);
+        });
+//        System.out.println("s");
+    }
+
+
+    //////////////////////// Race
+    // HouseCat < Donkey < Horse < Cheetah
+    static class Animal {
+        int speed;
+
+        public int getSpeed() {
+            return speed;
+        }
+    }
+
+    static class Equus extends Animal {
+    }
+
+    static class Horse extends Equus {
+        public Horse() {
+            speed = 88;
+        }
+    }
+
+    static class Donkey extends Equus {
+        public Donkey() {
+            speed = 24;
+        }
+    }
+
+    static class CrippleHorse extends Horse {
+        public CrippleHorse() {
+        }
+
+        @Inject
+        public void makeCripple() {
+            speed = 0;
+        }
+    }
+
+    static class Cat extends Animal {
+    }
+
+    static class Cheetah extends Cat {
+        public Cheetah() {
+            speed = 130;
+        }
+    }
+
+    static class HouseCat extends Cat {
+        public HouseCat() {
+            speed = 48;
+        }
+    }
+
+    public static class Race {
+        @Inject
+        public Equus equusContestant;
+        @Inject
+        public Cat catContestant;
+
+        public Race() {
+        }
+
+        ;
+//        Race(Equus equusContestant, Cat catContestant) {
+//            this.equusContestant = equusContestant;
+//            this.catContestant = catContestant;
+//        }
+
+
+        Animal getWinner() {
+            if (equusContestant.getSpeed() < catContestant.getSpeed())
+                return catContestant;
+            else
+                return equusContestant;
+        }
+
+        boolean didTheCatWin() {
+            return getWinner() == catContestant;
+        }
+    }
+
+    static class RaceTest {
+        Class equus, cat;
+        boolean doesTheCatWin;
+
+        RaceTest(Class equus, Class cat, boolean doesTheCatWin) {
+            this.equus = equus;
+            this.cat = cat;
+            this.doesTheCatWin = doesTheCatWin;
+        }
+    }
+
+    @Test
+    public void test_race_0() {
+
+        Injector i = new Injector();
+
+        RaceTest raceTests[] = {
+                // HouseCat < Donkey < Horse < Cheetah
+                new RaceTest(Horse.class, HouseCat.class, false),
+                new RaceTest(Donkey.class, HouseCat.class, true),
+                new RaceTest(Horse.class, Cheetah.class, true),
+                new RaceTest(Donkey.class, Cheetah.class, true),
+
+                // CrippleHorse always loses
+                new RaceTest(CrippleHorse.class, Cheetah.class, true),
+                new RaceTest(CrippleHorse.class, HouseCat.class, true),
+        };
+
+        for (RaceTest raceTest : raceTests) {
+            assertDoesNotThrow(() -> {
+                i.bind(Equus.class, raceTest.equus);
+                i.bind(Cat.class, raceTest.cat);
+
+                Race race = (Race) i.construct(Race.class);
+                assertEquals(raceTest.doesTheCatWin, race.didTheCatWin());
+            });
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
